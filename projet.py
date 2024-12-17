ad4d76adb4c7472f91c3ac6829c0ae2df7e493a7
axil0w <159565163+axil0w@users.noreply.github.com>
2024-12-17 11:49:15 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 import pandas as pd
-
+import sqlite3
 app = Flask(__name__)
 
-# Exemple de données pour le tableau
-donnees = [
-    {'nom': 'Jean Dupont', 'age': 35, 'ville': 'Paris'},
-    {'nom': 'Marie Martin', 'age': 28, 'ville': 'Lyon'},
-    {'nom': 'Pierre Durand', 'age': 42, 'ville': 'Marseille'},
-    {'nom': 'Sophie Leroy', 'age': 31, 'ville': 'Toulouse'}
-]
+
+connexion = sqlite3.connect("inventaire.db", check_same_thread=False)
+cursor = connexion.cursor()
+
+
+def get_all_items():
+    cursor.execute("SELECT * FROM stock")
+    return cursor.fetchall()
 
 @app.route('/')
 def afficher_tableau():
+    donnees = get_all_items()	
     return render_template('table.html', donnees=donnees)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
