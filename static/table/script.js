4e8ad65db4530e42ebe7f18dc5d41b0a9c57e8a1
axil0w <nathan.letestu@gmail.com>
2025-01-07 22:38:35 +0100

    chiant

--- static/table/script.js
+++ static/table/script.js
@@ -1,25 +1,36 @@
+function clearContents(element) {
+  element.value = '';
+}
+
+
 async function remove_object(object){
     //event.preventDefault();    // prevent page from refreshing
+    console.log(JSON.stringify([elemId,number]))
+    const root = object.parentElement.parentElement
+    const elemId = root.getAttribute("element")
+    var number = root.getElementById("remove").value
     let response = await fetch("/remove", {
             method: 'POST',
             mode: 'cors',
             headers: {
             'Content-Type': 'application/json'
            },
-            body: JSON.stringify(object)
+            body: JSON.stringify([elemId,number])
         });
     location.reload();
 }
 
 async function add_object(object){
     //event.preventDefault();    // prevent page from refreshing
+    const root = object.parentElement.parentElement
+    const elemId = root.getAttribute("element")
     let response = await fetch("/add", {
             method: 'POST',
             mode: 'cors',
             headers: {
             'Content-Type': 'application/json'
            },
-            body: JSON.stringify(object)
+            body: JSON.stringify(elemId)
         });
     location.reload();
 }
